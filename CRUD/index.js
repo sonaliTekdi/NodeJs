dde167deac064dec63d9eb3e9c9b8b6bf1852438
sonaligarud <sonali.garud@tekditechnologies.com>
2024-06-06 17:52:27 +0530

    added mongoDB

--- CRUD/index.js
+++ CRUD/index.js
@@ -4,18 +4,48 @@ const users = require("./MOCK_DATA.json");
 const app = express();
 const PORT = 8000;
 
+const mongoose = require("mongoose");
+//connection
+mongoose
+  .connect("mongodb://localhost:27017/myfirstdb")
+  .then(() => console.log("mongoDB connected"))
+  .catch((err) => console.log("MongoDB connection failed"));
+
+//schema
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  gender: {
+    type: String,
+  },
+  job_title: {
+    type: String,
+  },
+});
+
+const User = mongoose.model("user", userSchema);
+
 //Middleware
 app.use(express.urlencoded({ extended: false }));
 
 // Routes
-app.get("/api/users", (req, res) => {
-  return res.json(users);
+app.get("/api/users", async (req, res) => {
+  const allDbUsers = await User.find({});
+  console.log(allDbUsers, "allDbUsers");
 });
 
-app.get("/api/users/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const user = users.find((user) => user.id === id);
-
+app.get("/api/users/:id", async (req, res) => {
+  const user = await User.findById(req.params.id);
   if (user) {
     return res.json(user);
   } else {
@@ -23,12 +53,33 @@ app.get("/api/users/:id", (req, res) => {
   }
 });
 
-app.post("/api/users/", (req, res) => {
-  const body = req.body;
-  users.push({ ...body, id: users.length + 1 });
-  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-    return res.json({ status: "success", id: users.length });
-  });
+app.post("/api/users/", async (req, res) => {
+  try {
+    const body = req.body;
+    if (
+      !body ||
+      !body.firstName ||
+      !body.lastName ||
+      !body.email ||
+      !body.gender ||
+      !body.job_title
+    ) {
+      return res.status(400).json({ message: "All fields are mandatory" });
+    }
+    const result = await User.create({
+      firstName: body.firstName,
+      lastName: body.lastName,
+      email: body.email,
+      gender: body.gender,
+      job_title: body.job_title,
+    });
+    return res.status(201).json({ message: "Success" });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(409)
+      .json({ message: "user with this email id alrady exists." });
+  }
 });
 
 app.delete("/api/delete/users/:id", (req, res) => {
